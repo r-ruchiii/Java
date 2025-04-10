2af531eecc22ad0ed71438bf120e98c659cd8848
r-ruchiii <ruchi.jain@univest.in>
2025-04-10 16:17:14 +0530

    substring code

---
+++ substringmethod.java
@@ -0,0 +1,16 @@
+// Given a string,s , and two indices, start and end, print a substring consisting of all characters in the inclusive range from start to end-1 . 
+// You'll find the String class' substring method helpful in completing this challenge.
+
+import java.util.Scanner;
+
+public class substringmethod {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+
+        System.out.println(S.substring(start, end));
+        
+    }
+}
