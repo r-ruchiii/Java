98bf3267e0af0766d251f1fd67079683b83a6ad0
r-ruchiii <ruchi.jain@univest.in>
2025-04-11 16:21:19 +0530

    Palindrome String

---
+++ palindrome.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class palindrome {
+     public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        
+        // take string input
+        String A=sc.next();
+        
+        /* Enter your code here. Print output to STDOUT. */
+        
+        // store reverse
+        String rev = "";
+        
+        // Loop for rev string
+        for(int i = A.length()-1; i>=0; i--){
+            rev = rev + A.charAt(i);
+        }
+        
+        // check if rev == A
+        if(A.equals(rev)){
+            System.out.println("Yes");
+        }
+        else{
+            System.out.println("No");
+        }
+}
+}
