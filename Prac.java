b4ca59cd6d0a7dea5ac9911e52cb00e361fcb834
r-ruchiii <ruchi.jain@univest.in>
2025-06-17 12:26:29 +0530

    Java Code Practice

--- Prac.java
+++ Prac.java
@@ -1,22 +1,14 @@
-import java.util.Scanner;
-
 public class Prac{
     public static void main(String[] args) {
-         // System.out.println("Hello World");
+        System.out.println("Hello World");
         
-        // System.out.println(-5 + 8 * 6);
-        // System.out.println((55+9) % 9);
-        // System.out.println(20 + -3*5 / 8);
-        // System.out.println(5 + 15 / 3 * 2 - 8 % 3);
+        System.out.println(-5 + 8 * 6);
+        System.out.println((55+9) % 9);
+        System.out.println(20 + -3*5 / 8);
+        System.out.println(5 + 15 / 3 * 2 - 8 % 3);
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Input an integer: ");
-        int num = sc.nextInt();
+       System.out.println(((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5)));        
 
-        for (int i = 1; i<= 10; i++) {
-             System.out.println(num + " x " + i + " = " + (num * i));
+    }
 
-        }
-            
-        }
 }
---
+++ swap.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class swap {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        for (int i=0; i<2; i++){
+            System.out.println("Input an integer: ");
+            int var1 = sc.nextInt();
+            int var2 = sc.nextInt();
+
+            int temp = var1;
+            var1 = var2;
+            var2 = temp;
+
+            System.out.println("After Swapping -------->");
+            System.out.println("Number 1 : " + var1);
+            System.out.println("Number 2 : " + var2);
+
+        }
+        sc.close();
+    }
+}
