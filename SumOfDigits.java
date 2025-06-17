54abb1661aa28d69977121f8f30ae6998d9f24d6
r-ruchiii <ruchi.jain@univest.in>
2025-06-17 17:45:49 +0530

    Updated code

--- SumOfDigits.java
+++ SumOfDigits.java
@@ -4,8 +4,20 @@ public class SumOfDigits {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter number: ");
-        String num = sc.next();
+        long num = sc.nextLong();
 
+        System.out.println("Sum of the digits of given number i.e. " + num + " = " + sumDigits(num));
         
+    }
+
+    public static int sumDigits(long num){
+        int sum = 0;
+        
+        while(num != 0){
+            sum += num%10;
+            num /= 10;
+        }
+
+        return sum;
     }
 }
